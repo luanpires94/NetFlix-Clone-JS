21d661902f5a26a73250fce3fc7152845653efb2
vai na web <vainawebcpx@gmail.com>
2019-11-08 12:46:08 -0300

    inicio da main

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function search(){
-if (document.querySelector('input').style.display == 'none'){
-
-    document.querySelector('input').style.display = 'flex';
-    document.querySelector('input').style.border = '0.5px solid #FFF'
+if (document.querySelector('input').style.display == 'flex'){
+    document.querySelector('input').style.display = 'none';
+    document.querySelector('.interaction__box').style.border = 'none'
+    
  }
 
  else{
-    document.querySelector('input').style.display = 'none';
-    document.querySelector('input').style.border = 'none'
+    document.querySelector('input').style.display = 'flex';
+    document.querySelector('.interaction__box').style.border = 'solid 2px white';
  }
 
 }
